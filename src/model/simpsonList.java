b3b96a502f03c6dfe35ba7f77a6be4d56c36c77a
Nicolas Montero <nicolasmontero42@gmail.com>
2019-11-29 17:48:24 -0500

    Update simpsonList.java

--- src/model/simpsonList.java
+++ src/model/simpsonList.java
@@ -1,5 +1,28 @@
 package model;
 
+import java.util.LinkedList;
+
 public class simpsonList {
+	
+	private LinkedList<simpson> listaOriginal;
+	
+	public simpsonList() {
+		listaOriginal = new LinkedList<simpson>();
+	}
+	
+	public void adicionar(simpson simpson) {
+		listaOriginal.add(simpson);
+	}
+
+	public LinkedList<simpson> getListaOriginal() {
+		return listaOriginal;
+	}
+
+	public void setListaOriginal(LinkedList<simpson> listaOriginal) {
+		this.listaOriginal = listaOriginal;
+	}
+	
+	
+	
 
 }
